3f274a2257a17fd69296239506726c29ee158bcf
misha <mishadrakon@gmail.com>
2024-10-19 10:42:57 +0300

    first try to commit

---
+++ nw.js
@@ -0,0 +1,14 @@
+let storeName = 'Shop';
+const storeDescription = {
+    budget: 10000,
+    employees: [
+        'ana',
+        'Bob',
+        'dima'
+    ],
+    products:{
+        pc: 1000,
+        gpu: 5000
+    },
+    open: true ,
+};
---
+++ type.js
@@ -0,0 +1,7 @@
+"use strict";
+const answers = [];
+answers[0] = prompt('как ваше имя?', '');
+answers[0] = prompt('как ваша фамилия?', '');  
+answers[0] = +prompt('каков ваш возвраст?', '');
+
+document.write(answers);
